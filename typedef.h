1864522d4ebdd1d9ad621eb9785291bd75b171e5
l00sy4 <142809264+l00sy4@users.noreply.github.com>
2024-02-05 13:09:06 +0200

    Delete typedef.h
    No longer needed

--- typedef.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-typedef HWND(WINAPI* fnGetForegroundWindow)(void);
-
-typedef LRESULT(WINAPI* fnCallNextHookEx)(HHOOK hhk, int nCode, WPARAM wParam, LPARAM lParam);
-
-// Will populate more when the client is actually finished
